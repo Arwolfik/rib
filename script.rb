95b83d15f56ed78165c370421d0228e174c598f3
Arwolfik <107422886+Arwolfik@users.noreply.github.com>
2023-05-27 09:40:32 +0300

    Add files via upload

---
+++ script.rb
@@ -0,0 +1,8 @@
+# Задаем исходный массив из имён
+names = ["John", "Jane", "Alex", "Mary"]
+
+# Сортируем имена по алфавиту
+sorted_names = names.sort
+
+# Выводим отсортированные имена на экран
+puts sorted_names
